27f67213768eb07a0398c43bae6e2481ec0fd34b
Mackovic Daniel <mackovicdaniel99@gmail.com>
2022-08-29 16:47:09 +0200

    feat: color palette added

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      colors: {
+        back: "#302e31",
+        main: "#333332",
+        card: "#363438",
+        primary: "#0196ea",
+        succes: "#15803d",
+        error: "#b91c1c",
+        text: "#c7c6c7",
+        "text-disabled": "#d4d4d4",
+      },
     },
   },
   plugins: [],
