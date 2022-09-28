86532cc724042b86566357e430f5ea73275f3129
Mackovic Daniel <mackovicdaniel99@gmail.com>
2022-09-28 18:17:39 +0200

    refactor: itemcard created

--- components/wallets/Inventory.tsx
+++ components/wallets/Inventory.tsx
@@ -1,13 +1,8 @@
-import { motion } from "framer-motion";
-import Image from "next/image";
-import { useState } from "react";
 import { useWallet } from "../../pages/wallets";
-import Modal from "../global/modal/Modal";
-import ProductDetails from "./ProductDetails";
+import ItemCard from "./Inventory/ItemCard";
 
 export default function Inventory() {
   const { selected, selectedCategory, selectCategory } = useWallet();
-  const [isOpen, setIsOpen] = useState(null);
   return (
     <div className="flex h-full max-h-[500px] w-full flex-col gap-6 rounded-lg border border-border bg-back p-6">
       <div className="scrollbar w-full overflow-auto">
@@ -35,42 +30,14 @@ export default function Inventory() {
           {selectedCategory?.items &&
             selectedCategory?.items.map((item: any, index: number) => {
               return (
-                <div
-                  className="aspect-square basis-full p-1 sm:basis-1/3 lg:basis-1/3 xl:basis-1/2 2xl:basis-1/6"
+                <ItemCard
                   key={item._id}
-                >
-                  <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: index * 0.05 }}
-                    className="flex h-full flex-col items-center justify-center gap-2 rounded-lg border border-border bg-text p-5 text-xs font-medium text-text hover:cursor-pointer"
-                    onClick={() => setIsOpen(item)}
-                  >
-                    <div className="relative flex h-full w-full">
-                      <Image
-                        src={`/products/${item.product._id}.png`}
-                        objectFit="contain"
-                        layout="fill"
-                        alt="logo"
-                      />
-                    </div>
-                    {/* <div className="flex gap-1">
-                      <h2 className="text-secondary">
-                        {item.quantity + "" + item.product.quantityType}
-                      </h2>
-                      <h1>{item.product.name}</h1>
-                    </div> */}
-                  </motion.div>
-                </div>
+                  item={item}
+                  animationDelay={index * 0.05}
+                ></ItemCard>
               );
             })}
         </div>
-        <Modal isOpen={isOpen} size="max-w-md">
-          <ProductDetails
-            item={isOpen}
-            handleClose={() => setIsOpen(null)}
-          ></ProductDetails>
-        </Modal>
       </div>
     </div>
   );
---
+++ components/wallets/Inventory/ItemCard.tsx
@@ -0,0 +1,43 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { useState } from "react";
+import Modal from "../../global/modal/Modal";
+import ProductDetails from "../ProductDetails";
+
+interface ProductCardProps {
+  item: any;
+  animationDelay: number;
+}
+
+export default function ItemCard(props: ProductCardProps) {
+  const { item, animationDelay } = props;
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+  return (
+    <>
+      <div className="aspect-square basis-full p-1 sm:basis-1/3 lg:basis-1/3 xl:basis-1/2 2xl:basis-1/6">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: animationDelay }}
+          className="flex h-full flex-col items-center justify-center gap-2 rounded-lg border border-border bg-main p-5 text-xs font-medium text-text hover:cursor-pointer"
+          onClick={() => setIsDetailsOpen(item)}
+        >
+          <div className="relative flex h-full w-full">
+            <Image
+              src={`/products/${item.product._id}.png`}
+              objectFit="contain"
+              layout="fill"
+              alt="logo"
+            />
+          </div>
+        </motion.div>
+      </div>
+      <Modal isOpen={isDetailsOpen} size="max-w-md">
+        <ProductDetails
+          item={item}
+          handleClose={() => setIsDetailsOpen(false)}
+        ></ProductDetails>
+      </Modal>
+    </>
+  );
+}
