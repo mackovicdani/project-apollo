c52d312ff37521d02400a92bdbebb48af9427721
Mackovic Daniel <mackovicdaniel99@gmail.com>
2022-09-28 18:29:21 +0200

    refactor: categorycard created

--- components/wallets/Inventory.tsx
+++ components/wallets/Inventory.tsx
@@ -1,30 +1,18 @@
 import { useWallet } from "../../pages/wallets";
-import ItemCard from "./Inventory/ItemCard";
+import CategoryCard from "./inventory/CategoryCard";
+import ItemCard from "./inventory/ItemCard";
 
 export default function Inventory() {
-  const { selected, selectedCategory, selectCategory } = useWallet();
+  const { selected, selectedCategory } = useWallet();
   return (
-    <div className="flex h-full max-h-[500px] w-full flex-col gap-6 rounded-lg border border-border bg-back p-6">
+    <div className="flex h-full max-h-[500px] w-full flex-col rounded-lg border border-border bg-back p-6">
       <div className="scrollbar w-full overflow-auto">
         <div className="flex w-full flex-wrap">
           {!selectedCategory &&
             selected?.inventory &&
             selected?.inventory.map((category: any, index: number) => {
               if (category.items && category.items.length > 0) {
-                return (
-                  <div
-                    key={index}
-                    className="aspect-square basis-1/2 p-1 sm:basis-1/3 lg:basis-1/3 xl:basis-1/2 2xl:basis-1/6"
-                    onClick={() => selectCategory(category)}
-                  >
-                    <div className="flex h-full flex-col items-center justify-center rounded-lg border border-border bg-secondary text-xs font-bold text-text">
-                      <h1 className="text-center text-xs font-medium uppercase">
-                        {category.name}
-                      </h1>
-                      <h2 className="text-xs">{category.items.length}</h2>
-                    </div>
-                  </div>
-                );
+                return <CategoryCard category={category} />;
               }
             })}
           {selectedCategory?.items &&
---
+++ components/wallets/inventory/CategoryCard.tsx
@@ -0,0 +1,22 @@
+import { useWallet } from "../../../pages/wallets";
+
+interface CategoryCardProps {
+  category: any;
+}
+export default function CategoryCard(props: CategoryCardProps) {
+  const { category } = props;
+  const { selectCategory } = useWallet();
+  return (
+    <div
+      className="aspect-square basis-1/2 p-1 sm:basis-1/3 lg:basis-1/3 xl:basis-1/2 2xl:basis-1/6"
+      onClick={() => selectCategory(category)}
+    >
+      <div className="flex h-full flex-col items-center justify-center rounded-lg border border-border bg-secondary text-xs font-bold text-text">
+        <h1 className="text-center text-xs font-medium uppercase">
+          {category.name}
+        </h1>
+        <h2 className="text-xs">{category.items.length}</h2>
+      </div>
+    </div>
+  );
+}
