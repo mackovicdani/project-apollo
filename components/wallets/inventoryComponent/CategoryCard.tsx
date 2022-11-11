50aef782d525b6bb9936faedac77bff7c0adf2bc
Mackovic Daniel <mackovicdaniel99@gmail.com>
2022-11-11 22:34:24 +0100

    feat: purchase details card design updated

--- components/wallets/inventoryComponent/CategoryCard.tsx
+++ components/wallets/inventoryComponent/CategoryCard.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useWallet } from "../../../pages/wallets";
 
 interface CategoryCardProps {
@@ -7,15 +8,15 @@ export default function CategoryCard(props: CategoryCardProps) {
   const { category } = props;
   const { selectCategory } = useWallet();
   return (
-    <div
+    <motion.div
       className="aspect-square basis-1/2 p-1 sm:basis-1/4 lg:basis-1/6 xl:basis-1/4 2xl:basis-1/6"
       onClick={() => selectCategory(category)}
     >
-      <div className="relative flex h-full flex-col items-center justify-center overflow-hidden rounded-lg border border-border bg-gradient-to-tl from-main via-primary-main to-main">
-        <h1 className="absolute -rotate-45 text-center text-[1.5rem] font-bold uppercase text-text">
+      <div className="group relative flex h-full flex-col items-center justify-center overflow-hidden rounded-lg border border-border bg-gradient-to-tl from-main via-primary-main to-main hover:cursor-pointer">
+        <h1 className="absolute -rotate-45 text-center text-[1.5rem] font-bold uppercase text-text transition-all group-hover:text-[1.55rem]">
           {category.name}
         </h1>
       </div>
-    </div>
+    </motion.div>
   );
 }
--- components/wallets/sideComponenet/purchase/PurchaseDetails.tsx
+++ components/wallets/sideComponenet/purchase/PurchaseDetails.tsx
@@ -4,24 +4,32 @@ import Image from "next/image";
 
 export default function PurchaseDetails(props: any) {
   return (
-    <div className="flex max-w-md flex-col text-text">
-      <div className="p-7">
-        <div className="">
-          <h1 className=" text-lg">{props.purchase.user.name}</h1>
-          <h2 className="text-xs">
-            {moment(props.purchase.date)
-              .locale("hun")
-              .format("YYYY.MM.DD HH:mm:ss")}
-          </h2>
-          <h2 className="text-xs">{props.purchase.store.name}</h2>
+    <div>
+      <div className="p-3 text-text">
+        <div className="flex flex-col items-center">
+          <h1 className="text-center text-2xl font-extrabold text-secondary">
+            Purchase details
+          </h1>
+          <div className="flex w-full flex-col items-center py-1">
+            <p className="text-base font-semibold">
+              {props.purchase.user.name}
+            </p>
+            <p className="text-xs">
+              {moment(props.purchase.date)
+                .locale("hun")
+                .format("YYYY.MM.DD HH:mm:ss")}
+            </p>
+            <p className="text-xs">{props.purchase.store.name}</p>
+          </div>
         </div>
-        <div className="scrollbar mt-3 flex h-full max-h-96 flex-col flex-nowrap gap-2 overflow-y-auto overflow-x-hidden border-b-[1px] border-elev p-3">
+        <div className="scrollbar mt-3 flex h-full max-h-96 flex-col flex-nowrap gap-2 overflow-y-auto overflow-x-hidden border-b border-main p-3">
           {props.purchase.items.length > 0 &&
             props.purchase.items.map((item: any, index: number) => {
               return (
                 <motion.div
-                  initial={{ x: 100 }}
-                  animate={{ x: 0 }}
+                  initial={{ scale: 1.01 }}
+                  animate={{ scale: 1 }}
+                  transition={{ delay: 0.2 }}
                   key={index}
                   className=" relative flex h-10 min-h-[2.5rem] w-full rounded text-text"
                 >
@@ -36,7 +44,10 @@ export default function PurchaseDetails(props: any) {
                     </div>
                   </div>
                   <div className="flex flex-col">
-                    <h2 className="ml-2">{item.product.name}</h2>
+                    <h2 className="ml-2">
+                      {item.product.name}{" "}
+                      {item.product.packageSize + item.product.quantityType}
+                    </h2>
                     <h2 className="ml-3 -mt-1 text-xs font-bold text-secondary">
                       {item.product.category.toUpperCase()}
                     </h2>
@@ -61,19 +72,19 @@ export default function PurchaseDetails(props: any) {
         </div>
         <div className="p-3 text-white">
           <h2 className="text-end">
-            {props.purchase.price}
-            <span className="text-xs font-bold text-text-disabled">ft</span>
+            <span className="text-xs">Sum:</span> {props.purchase.price}
+            <span className="text-xs font-bold text-text-disabled"> ft</span>
           </h2>
         </div>
       </div>
 
-      <div className="flex h-20 w-full items-center justify-end gap-3 p-10">
+      <div className="flex h-16 w-full items-center justify-end gap-3 border-t border-border bg-dark p-3">
         <button
           type="button"
           onClick={() => {
             props.handleClose();
           }}
-          className="h-10 w-24 rounded-md border border-border bg-main text-sm text-text"
+          className="h-10 w-24 rounded-md border border-border bg-back text-sm text-text"
         >
           Close
         </button>
